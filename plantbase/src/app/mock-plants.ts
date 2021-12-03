46ba82458af42c7ea3814935f5a651965b6e298f
Petra Knip <petra@degrosolutions.nl>
2021-12-03 15:41:32 +0100

    basic grid toegevoegd

--- plantbase/src/app/mock-plants.ts
+++ plantbase/src/app/mock-plants.ts
@@ -1,14 +1,14 @@
 import { Plant } from './plant';
 
 export const PLANTS: Plant[] = [
-  { id: 1, name: 'Plant 1' },
-  { id: 2, name: 'Plant 2' },
-  { id: 3, name: 'Plant 3' },
-  { id: 4, name: 'Plant 4' },
-  { id: 5, name: 'Plant 5' },
-  { id: 6, name: 'Plant 6' },
-  { id: 7, name: 'Plant 7' },
-  { id: 8, name: 'Plant 8' },
-  { id: 9, name: 'Plant 9' },
-  { id: 10, name: 'Plant 10' },
+  { id: 1, src: '/assets/images/plants.jpg', name: 'Plant 1' },
+  { id: 2, src: './assets/images/plants.jpg', name: 'Plant 2' },
+  { id: 3, src: './assets/images/plants.jpg', name: 'Plant 3' },
+  { id: 4, src: './assets/images/plants.jpg', name: 'Plant 4' },
+  { id: 5, src: './assets/images/plants.jpg', name: 'Plant 5' },
+  { id: 6, src: './assets/images/plants.jpg', name: 'Plant 6' },
+  { id: 7, src: './assets/images/plants.jpg', name: 'Plant 7' },
+  { id: 8, src: './assets/images/plants.jpg', name: 'Plant 8' },
+  { id: 9, src: './assets/images/plants.jpg', name: 'Plant 9' },
+  { id: 10, src: './assets/images/plants.jpg', name: 'Plant 10' },
 ];
--- plantbase/src/app/plant.ts
+++ plantbase/src/app/plant.ts
@@ -1,4 +1,5 @@
 export interface Plant {
   id: number;
   name: string;
+  src: any;
 }
